bd90b7e0fc42394312e711421870436737306402
David Pérez <91068383+x86david@users.noreply.github.com>
2026-05-05 17:11:24 +0200

    Add shebang and update extensions installation script

---
+++ extensions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update && sudo apt install \
+gnome-shell-extension-tiling-assistant \
+gnome-shell-extension-search-light \
+gnome-shell-extension-desktop-clock \
+gnome-shell-extension-desktop-icons-ng \
+gnome-shell-extension-dashtodock \
+gnome-shell-extension-dash-to-panel \
+gnome-shell-extension-blur-my-shell \
+gnome-shell-extension-appindicator \
+gnome-shell-extension-arc-menu \
+gnome-shell-extension-caffeine \
+gnome-shell-extension-system-monitor \
+gnome-shell-extension-gsconnect \
+gnome-shell-extension-gpaste \
+gnome-shell-extension-manager \
+gir1.2-gnomedesktop-3.0 gir1.2-gnomedesktop-4.0
